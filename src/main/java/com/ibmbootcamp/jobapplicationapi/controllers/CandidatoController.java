1915ed14ec49a1fac1035c887f7e94e3521b7d86
luc0liv <luoliveiradacunha@gmail.com>
2023-07-11 16:20:04 -0300

    feat: add CrossOrigin annotation

--- src/main/java/com/ibmbootcamp/jobapplicationapi/controllers/CandidatoController.java
+++ src/main/java/com/ibmbootcamp/jobapplicationapi/controllers/CandidatoController.java
@@ -5,16 +5,19 @@ import com.ibmbootcamp.jobapplicationapi.services.CandidatoService;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/api/v1/hiring")
+@CrossOrigin(origins = "http://localhost:4200/", methods = {RequestMethod.GET, RequestMethod.POST})
 public class CandidatoController {
   private final CandidatoService candidatoService;
 
